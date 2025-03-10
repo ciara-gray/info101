6b7dcb9017db08ad6e9cd1b637dff413381dc6a6
ciara-gray <ciaragray@ucsb.edu>
2025-03-10 12:57:50 -0700

    updates

--- exercises/exercises101b.R
+++ exercises/exercises101b.R
@@ -4,9 +4,15 @@ rovs <- data.frame(
   price_usd = c(3459, 2999, 6500)
 )
 
+# Two columns, three rows
+View(rovs)
+
+
 # Fill in the blanks to create a data frame with columns "species" and "mass_kg"
 # The lingcod weighed 4.9 kg and the two halibut weighed 11.2 and 14.1 kg.
 harvest <- data.frame(
-  ??? = c("halibut", "lingcod", "halibut"),
-  mass_kg = c(14.1, ???, ???)
+  species = c("halibut", "lingcod", "halibut"),
+  mass_kg = c(14.1, 4.9, 11.2)
 )
+
+View(harvest)
--- info101.R
+++ info101.R
@@ -7,21 +7,28 @@ library(marinecs100b)
 # P1 Call the function dir() at the console. This lists the files in your
 # project's directory. Do you see woa.csv in the list? (If you don't, move it to
 # the right place before proceeding.)
-
+dir()
 
 # P2 Critique the organization of woa.csv according to the characteristics of
 # tidy data.
 
-
+# I'm not a computer and I'm confused. The data is not a rectangle, some titles
+# span more than one column and some columns are missing cells.
 
 # Importing data ----------------------------------------------------------
 
 # P3 P3 Call read.csv() on woa.csv. What error message do you get? What do you
 # think that means?
+read.csv("woa.csv")
 
+# "Error in read.table(file = file, header = header, sep = sep, quote = quote,
+# : more columns than column names"
+# I think this means there is data in columns that are not titled.
+?read.csv
 
 # P4 Re-write the call to read.csv() to avoid the error in P3.
-
+read.table("woa.csv", header = FALSE)
+read.csv("woa.csv", skip = 1)
 
 
 # Fix the column names ----------------------------------------------------
@@ -30,27 +37,36 @@ library(marinecs100b)
 
 depths <- c(
   seq(0, 100, by = 5),
-  seq(???, ???, by = 25),
-  seq(???, ???, by = ???),
-  seq(???)
+  seq(125, 500, by = 25),
+  seq(550, 2000, by = 50),
+  seq(2100, 5500, by = 100)
 )
 
+?seq
+depths
 
 # P6 Create a vector called woa_colnames with clean names for all 104 columns.
 # Make them the column names of your WOA data frame.
 
-
+woa_colnames <- c("Latitude", "Longitude", paste0("Depth ", depths))
+paste0("Depth ", depths)
+?paste
 
 # Analyzing wide-format data ----------------------------------------------
 
 # P7 What is the mean water temperature globally in the twilight zone (200-1000m
 # depth)?
 
-
+depths[25]
+depths[47]
+x <- c(depths[25] : depths[47])
+mean(x)
+?mean
 
 # Analyzing long-format data ----------------------------------------------
 
-# P8 Using woa_long, find the mean water temperature globally in the twilight zone.
+# P8 Using woa_long, find the mean water temperature globally in the twilight
+# zone.
 
 
 # P9 Compare and contrast your solutions to P8 and P9.
@@ -60,7 +76,9 @@ depths <- c(
 # location nearest to the coordinates listed in the in-class instructions.
 
 
-# P11 Interpret your temperature-depth profile. What's the temperature at the surface? How about in the deepest parts? Over what depth range does temperature change the most?
+# P11 Interpret your temperature-depth profile. What's the temperature at the
+# surface? How about in the deepest parts? Over what depth range does
+# temperature change the most?
 
 # ggplot is a tool for making figures, you'll learn its details in COMM101
 ggplot(mariana_temps, aes(temp_c, depth_m)) +
